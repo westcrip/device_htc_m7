257d5cab7620ed8a33b202798de66ce83bfdf662
altan <korukaltan@gmail.com>
2013-06-29 21:09:12 +0300

    m7 common device updates
    Change-Id: I10ebde543b9e87373be5ae2f106c5ecb9b1407c5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,7 @@ COMMON_GLOBAL_CFLAGS += -DNEW_LIBRIL_HTC
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
 BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recog/camcorder use cases
 BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
-TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+BOARD_AUDIO_AMPLIFIER := device/htc/m7-common/libaudioamp
 BOARD_HAVE_HTC_CSDCLIENT := true
 
 # Bluetooth
@@ -94,6 +94,12 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27917287424
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Custom Recovery
+ifneq ($(filter m7att m7tmo m7ul,$(TARGET_DEVICE)),)
+TARGET_RECOVERY_FSTAB := device/htc/m7-common/recovery/recovery.fstab.gsm
+else
+TARGET_RECOVERY_FSTAB := device/htc/m7-common/recovery/recovery.fstab.cdma
+endif
+BOARD_CUSTOM_GRAPHICS := ../../../device/htc/m7-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
--- m7-common.mk
+++ m7-common.mk
@@ -14,23 +14,29 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # common msm8960 configs
 $(call inherit-product, device/htc/msm8960-common/msm8960.mk)
 
 # overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Ramdisk
 PRODUCT_PACKAGES += \
-    fstab.target \
+    fstab.qcom \
     init.qcom.firmware_links.sh \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    remount.target \
+    remount.qcom \
     ueventd.qcom.rc
+
+# Post boot service
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.post_boot.sh:system/etc/init.post_boot.sh
 
 # Custom recovery charging
 PRODUCT_COPY_FILES += \
@@ -55,6 +61,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calibration:/system/etc/calibration \
     $(LOCAL_PATH)/configs/calibration_EMEA:/system/etc/calibration_EMEA
+
+# Audio config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Sound configs
 PRODUCT_COPY_FILES += \
@@ -84,6 +94,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/projector_input.idc:system/usr/idc/projector_input.idc \
     $(LOCAL_PATH)/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
 
+# Audio
+PRODUCT_PACKAGES += \
+    libaudioamp
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8960
@@ -97,6 +111,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    nfc.msm8960 \
     libnfc \
     libnfc_ndef \
     libnfc_jni \
@@ -112,7 +127,18 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := device/htc/m7-common/configs/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := device/htc/m7-common/configs/nfcee_access_debug.xml
+endif
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb
@@ -123,6 +149,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.nfc.fw_boot_download=false \
     debug.nfc.se=true \
     ro.nfc.port=I2C \
+    ro.sf.lcd_density=480 \
     persist.timed.enable=true \
     persist.gps.qmienabled=true \
     ro.baseband.arch=mdm
@@ -137,5 +164,8 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi xxhdpi
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/htc/m7-common/common-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/m7-common/m7-common-vendor.mk)
